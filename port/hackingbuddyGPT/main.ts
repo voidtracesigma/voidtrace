6d156c10cc419c43c80b7ace997008dd514b8c52
voidtrace <voidtrace_sigma@proton.me>
2026-02-14 18:41:29 +0800

    add structure modelling for existing work

---
+++ port/hackingbuddyGPT/main.ts
@@ -0,0 +1,76 @@
+// cli/wintermute.py -> utils/configurable.py:instantiate.run()
+    // from hackingBuddyGPT.usecases.base import use_cases
+
+
+
+    
+capabilities/
+    __init__.py
+    capability.py
+    http_request.py
+    local_shell.py
+    parsed_information.py
+    psexec_run_command.py
+    psexec_test_credential.py
+    python_test_case.py
+    record_note.py
+    ssh_run_command.py
+    ssh_test_credential.py
+    submit_flag.py
+    submit_http_method.py
+    yamlFile.py
+
+utils/
+    console
+    db_storage
+    local_shell
+    openai
+    prompt_generation
+    psexec
+    ssh_connection
+    __init__.py
+    cli_history.py
+    configurable.py
+    llm_util.py
+    logging.py
+    shell_root_detection.py
+    ui.py
+
+usecases/
+    examples/
+    examples/__init__.py
+    examples/agent_with_state.py
+    examples/agent.py
+    examples/get_hint_from_lse.txt
+    examples/hintfile.py
+    examples/lse.py
+    examples/next_cmd.txt
+    privesc
+    privesc/templates
+    privesc/__init__.py
+    privesc/common.py
+    privesc/linux.py
+    privesc/windows.py
+    rag
+    rag/rag_storage
+    rag/templates
+    rag/__init__.py
+    rag/common.py
+    rag/linux.py
+    rag/rag_utility.py
+    rag/README.md
+    web
+    web/__init__.py
+    web/with_explanation.py
+    web_api_testing
+    web_api_testing/documentation
+    web_api_testing/response_processing
+    web_api_testing/testing
+    web_api_testing/utils
+    web_api_testing/__init__.py
+    web_api_testing/simple_openapi_documentation.py
+    web_api_testing/simple_web_api_testing.py
+__init__.py
+agents.py
+base.py
+viewer.py
